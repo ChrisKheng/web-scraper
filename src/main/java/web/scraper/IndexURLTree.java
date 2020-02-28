17637b18a8f15a227c4ed63d75871a16a4e02bb2
Kok Jian Yu <kokjianyu@gmail.com>
2020-02-28 19:53:22 +0800

    add skeleton code for IndexURLTree

---
+++ src/main/java/web/scraper/IndexURLTree.java
@@ -0,0 +1,19 @@
+package web.scraper;
+
+public class IndexURLTree {
+
+    public IndexURLTree() {
+
+    }
+
+    public void addURLandContent() {
+        //TODO: Add URL and Content passed to this method to the tree
+    }
+
+    public void isDuplicate() {
+        //TODO: Check if URL is already stored
+    }
+
+
+
+}
