943d2c36fcf5755e04bda1b47a0cdd59ed3b1034
Kok Jian Yu <kokjianyu@gmail.com>
2020-02-29 13:48:38 +0800

    add basic implementation outline of IndexBuilder methods.

--- src/main/java/web/scraper/IndexBuilder.java
+++ src/main/java/web/scraper/IndexBuilder.java
@@ -1,18 +1,43 @@
 package web.scraper;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.logging.Logger;
+
 public class IndexBuilder extends Thread {
 
+    private TreeSet<String> tree;
+    private LinkedList<String> buffer;
+    private Logger logger = Logger.getLogger("IndexBuilder");
 
-  public IndexBuilder(){
-  }
+    // I put everything for now, but probably only need buffer and IUT.
+    public IndexBuilder(TreeSet<String> tree, List<String> buffer) {
+        this.tree = tree;
+        this.buffer = (LinkedList<String>) buffer;
+    }
 
-  @Override
-  public void run() {
+    @Override
+    public void run() {
+        // TODO: add some sort of check to prevent busy waiting. Probably semaphore or something.
+        while (true) {
+            logger.info(Integer.toString(buffer.size()));
+            if (buffer.size() > 0) {
+                String data = readBUL();
+                writeIUT(data);
+            }
+        }
+    }
 
-  }
+    public String readBUL() {
+        String data = buffer.removeFirst();
+        return data;
+    }
 
-  public void readBUL(){
+    public void writeIUT(String data) {
+        //TODO: Write code
+        // should be simply calling the write method of IndexURLTree
 
-  }
+    }
 
 }
