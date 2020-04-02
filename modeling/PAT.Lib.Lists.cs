3a094b59d28c8c52e6e0dd4e71a3c6382752ee2e
Chris Kheng <chriskheng1609@gmail.com>
2020-04-02 21:52:29 +0800

    Fix bugs in C# custom Lists data structure

--- modeling/PAT.Lib.Lists.cs
+++ modeling/PAT.Lib.Lists.cs
@@ -6,94 +6,102 @@ using PAT.Common.Classes.Expressions.ExpressionClass;
 
 //the namespace must be PAT.Lib, the class and method names can be arbitrary
 namespace PAT.Lib
-{
-    /// <summary>
-    /// The math library that can be used in your model.
-    /// all methods should be declared as public static.
-    /// 
-    /// The parameters must be of type "int", or "int array"
-    /// The number of parameters can be 0 or many
-    /// 
-    /// The return type can be bool, int or int[] only.
-    /// 
-    /// The method name will be used directly in your model.
-    /// e.g. call(max, 10, 2), call(dominate, 3, 2), call(amax, [1,3,5]),
-    /// 
-    /// Note: method names are case sensetive
-    /// </summary>
+{    	
+    // Lists is a class which models an array of lists.
+    // It can be used to model the collection of queues of crawler and 
+    // the collection of buffers.        
     public class Lists : ExpressionValue
     {
-		public List<int>[] lists;
+        public List<int>[] lists;
+        public int[] a;
 
-		public Lists(int numLists) {
-			this.lists = new List<int>[numLists];
-		}
-
-		public Lists(List<int>[] lists) {
-			List<int>[] clone = new List<int>[lists.Length];
-			
-			for (int i = 0; i < lists.Length; i++) {
-				clone[i] = lists[i].ToList();
-			}
-			
-			this.lists = clone;
-		}
-
-		public void AddToList(int listNumber, int url) {
-			lists[listNumber].Add(url);
-		}
-		
-		public int PollFromList(int listNumber) {
-			int head = lists[listNumber][0];
-			lists[listNumber].RemoveAt(0);
-			
-			return head;
-		}
-		
-		public bool isListEmpty(int listNumber) {
-			return !(lists[listNumber].Any());
-		}
-
-        /// <summary>
-        /// Please implement this method to provide the string representation of the datatype
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return ExpressionID;
+        // Needs a default constructor.
+        public Lists() {
+            this.lists = new List<int>[0];
         }
 
+        // numLists: number of lists in the array.
+        public Lists(int numLists) {
+            this.lists = new List<int>[numLists];
 
-        /// <summary>
-        /// Please implement this method to return a deep clone of the current object
-        /// </summary>
-        /// <returns></returns>
+            // Initialise every element in the list
+            for (int i = 0; i < numLists; i++) {
+                this.lists[i] = new List<int>();
+            }
+        }
+
+        // lists: an array of lists to be deep copied into the new Lists object.
+        public Lists(List<int>[] lists) {
+            List<int>[] clone = new List<int>[lists.Length];
+            
+            for (int i = 0; i < lists.Length; i++) {
+                // copy the list and add it to the array
+                clone[i] = lists[i].ToList();
+            }
+            
+            this.lists = clone;
+        }
+
+        // Appends all the elements in the given array to the end of the 
+        // target list.
+        // listNumber: the index of the target list.        
+        // Needs to return something for PAT code to work (i.e. cannot return void)
+        public bool Append(int listNumber, int[] array) {
+            this.lists[listNumber].AddRange(array);
+			return true;
+        }
+
+        // Adds the given url into the target list.        
+        // listNumber: the index of the target list. (index starts from 0)
+        // url: the url to be added into the target list.        
+        public void AddToList(int listNumber, int url) {
+            lists[listNumber].Add(url);
+        }
+        
+        // Removes and returns the first url from the target list.
+        // listNumber: the index of the target list.
+        public int PollFromList(int listNumber) {
+            int head = lists[listNumber][0];
+            lists[listNumber].RemoveAt(0);
+            
+            return head;
+        }
+        
+        // Returns true if the target list is empty, otherwise false.
+        // listNumber: the index of the target list.
+        public bool isListEmpty(int listNumber) {
+            return !(lists[listNumber].Any());
+        }
+                
+        //---------------------------- For system use ------------------------------
+        /// Returns the  string representation of the datatype.
+        public override string ToString() {
+            return ExpressionID;
+        }
+        
+        /// Returns a deep clone of the current object.
         public override ExpressionValue GetClone()
         {
             return new Lists(this.lists);
         }
-
-
-        /// <summary>
-        /// Please implement this method to provide the compact string representation of the datatype
-        /// </summary>
-        /// <returns></returns>
+        
+        /// Returns the compact string representation of the datatype.
         public override string ExpressionID
         {
             get {
-            	String result = "";
-            	
-            	foreach (List<int> list in this.lists) {
-            		result += "[" + String.Join(", ", list) + "], ";
-            	}
-            	
-            	if (result.Length > 0) {
-            		result.Remove(result.Length - 2, 2);
-            	}
-            	
-            	return result; 
+                String result = "";
+                
+                foreach (List<int> list in this.lists) {
+                    result += "[" + String.Join(", ", list) + "], ";
+                }
+                
+               if (result.Length > 0) {
+                   // Removes the ',' at the end and the trailing space.
+                   result = result.Remove(result.Length - 2, 2);
+               }
+                
+                return result; 
             }
         }
-
     }
 }
