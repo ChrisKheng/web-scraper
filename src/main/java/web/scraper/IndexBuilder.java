3b37aff542e9d59a2f54e17b0ebfcf800c99e5f1
Kok Jian Yu <kokjianyu@gmail.com>
2020-04-09 15:44:58 +0800

    auto indent

--- src/main/java/web/scraper/IndexBuilder.java
+++ src/main/java/web/scraper/IndexBuilder.java
@@ -5,6 +5,7 @@ import java.util.concurrent.Semaphore;
 import java.util.logging.Logger;
 
 public class IndexBuilder extends CustomThread {
+
     private IndexURLTree tree;
     private List<Data> buffer;
     private Semaphore crawlerSemaphore;
@@ -12,7 +13,8 @@ public class IndexBuilder extends CustomThread {
     private Logger logger = Logger.getLogger("IndexBuilder");
     private long count; // For keep tracking the number of urls added to the tree (temporal implementation)
 
-    public IndexBuilder(IndexURLTree tree, List<Data> buffer, Semaphore crawlerSemaphore, Semaphore builderSemaphore) {
+    public IndexBuilder(IndexURLTree tree, List<Data> buffer, Semaphore crawlerSemaphore,
+        Semaphore builderSemaphore) {
         this.tree = tree;
         this.buffer = buffer;
         this.crawlerSemaphore = crawlerSemaphore;
@@ -22,19 +24,20 @@ public class IndexBuilder extends CustomThread {
 
     @Override
     public void run() {
-        super.setThreadName(String.format("Builder %d", Thread.currentThread().getId()));        
+        super.setThreadName(String.format("Builder %d", Thread.currentThread().getId()));
 
         while (!this.isInterrupted()) {
             try {
                 builderSempahore.acquire(App.BUFFER_SIZE);
                 logger.info(getFormattedMessage("Entering critical section............."));
-                logger.info(getFormattedMessage(String.format("buffer size is %d .........", buffer.size())));
+                logger.info(getFormattedMessage(
+                    String.format("buffer size is %d .........", buffer.size())));
 
                 while (!buffer.isEmpty()) {
                     Data data = buffer.remove(0);
-                    writeIUT(data);        
+                    writeIUT(data);
                 }
-                
+
                 crawlerSemaphore.release(App.BUFFER_SIZE);
                 logger.info(getFormattedMessage("Leaving critical section............."));
             } catch (InterruptedException e) {
