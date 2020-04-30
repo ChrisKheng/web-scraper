4c5575fff794f4e4dfada817c8325060e689ba02
Chris Kheng <chriskheng1609@gmail.com>
2020-04-30 18:56:26 +0800

    Remove replacement of newline characters in Data

--- src/main/java/web/scraper/Data.java
+++ src/main/java/web/scraper/Data.java
@@ -16,8 +16,8 @@ class Data {
     }
 
     private Data(String sourceUrl, String newUrl, String document, boolean isNewUrlDead) {
-        this.sourceUrl = sourceUrl.replace("\n", "\\n").replace("\r", "\\r");
-        this.newUrl = newUrl.replace("\n", "\\n").replace("\r", "\\r");
+        this.sourceUrl = sourceUrl;
+        this.newUrl = newUrl;
         this.document = document;
         this.isNewUrlDead = isNewUrlDead;
     }
