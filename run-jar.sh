d6b1d537dddb1e8cf8dfc0d8301525aa8aae129e
Chris Kheng <chriskheng1609@gmail.com>
2020-05-01 18:19:40 +0800

    Update run-jar.sh

--- run-jar.sh
+++ run-jar.sh
@@ -11,4 +11,4 @@ rm -r data
 cd build/libs
 
 # Run the jar file
-java -jar crawler.jar -time 1h -input seed.txt -output res.txt
+java -jar crawler.jar -time 1h -input seed.txt -output res.txt -storedPageNum 1000
